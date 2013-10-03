f95d8da07d744aaef8e36c77d579509adcb9415c
Rui Figueira <rui.figueira@gmail.com>
2013-10-03 18:57:16 +0100

    ConditionWebElements if* methods renamed to when*

--- minium-core/src/main/java/com/vilt/minium/ConditionWebElements.java
+++ minium-core/src/main/java/com/vilt/minium/ConditionWebElements.java
@@ -18,13 +18,13 @@ package com.vilt.minium;
 @JQueryResources("minium/js/condition.js")
 public interface ConditionWebElements<T extends CoreWebElements<T>> extends WebElements {
 
-	public T whenNotEmpty(String selector);
+	public T ifNotEmpty(String selector);
 	
-	public T whenNotEmpty(T elems);
+	public T ifNotEmpty(T elems);
 	
-	public T whenEmpty(String elems);
+	public T ifEmpty(String elems);
 
-	public T whenEmpty(T elems);
+	public T ifEmpty(T elems);
 
 	public T notEmptyThen(String selector);
 	
