2666aaadb5b1b357db2928f381941547133d94fd
Rui Figueira <rui.figueira@gmail.com>
2013-10-06 23:20:40 +0100

    Minium extension resources folder renamed

--- minium-webconsole/src/main/java/com/vilt/minium/webconsole/SelectorGadgetWebElements.java
+++ minium-webconsole/src/main/java/com/vilt/minium/webconsole/SelectorGadgetWebElements.java
@@ -4,8 +4,8 @@ import com.vilt.minium.JQueryResources;
 import com.vilt.minium.WebElements;
 
 @JQueryResources(
-        value = { "rhino/selectorgadget/selectorgadget_combined.js", "rhino/selectorgadget/selectorgadget_minium.js" },
-        styles = "rhino/selectorgadget/selectorgadget_combined.css"
+        value = { "minium/selectorgadget/selectorgadget_combined.js", "minium/selectorgadget/selectorgadget_minium.js" },
+        styles = "minium/selectorgadget/selectorgadget_combined.css"
 )
 public interface SelectorGadgetWebElements extends WebElements {
     public void activateSelectorGadget();
