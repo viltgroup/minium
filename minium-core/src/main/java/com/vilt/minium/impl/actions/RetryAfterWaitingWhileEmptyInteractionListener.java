bc41664b7010ef1c147431f3c3869f362ecf1125
Rui Figueira <rui.figueira@gmail.com>
2014-03-14 16:08:18 +0000

    This listener allows Minium to wait when a timeout occurs because some element is not yet visible

---
+++ minium-core/src/main/java/com/vilt/minium/impl/actions/RetryAfterWaitingWhileEmptyInteractionListener.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2013 The Minium Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.vilt.minium.impl.actions;
+
+import static com.vilt.minium.actions.Interactions.withWaitingPreset;
+import static com.vilt.minium.actions.Interactions.withoutWaiting;
+
+import com.vilt.minium.CoreWebElements;
+import com.vilt.minium.TimeoutException;
+
+public class RetryAfterWaitingWhileEmptyInteractionListener extends DefaultInteractionListener {
+
+    private final CoreWebElements<?> waitElements;
+    private final String waitingPreset;
+
+    public RetryAfterWaitingWhileEmptyInteractionListener(CoreWebElements<?> source, String waitingPreset) {
+        this.waitElements = source;
+        this.waitingPreset = waitingPreset;
+    }
+
+    @Override
+    protected void onAfterFailEvent(AfterFailInteractionEvent event) {
+        if (event.getException() instanceof TimeoutException && withoutWaiting().checkEmpty(waitElements)) {
+            try {
+                withWaitingPreset(waitingPreset).waitWhileEmpty(waitElements);
+                event.setRetry(true);
+            } catch (Exception e) {
+                // we tried...
+            }
+        }
+    }
+}
