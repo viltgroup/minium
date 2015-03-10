9cc4f04e96ba0bebdf8d2f353ee88e6ff8ce650b
Rui Figueira <rui.figueira@vilt-group.com>
2015-03-10 08:58:40 +0000

    WaitInteractable now documented Base elements expression pattern documented

--- minium-actions/src/main/java/minium/actions/WaitInteractable.java
+++ minium-actions/src/main/java/minium/actions/WaitInteractable.java
@@ -17,24 +17,97 @@ package minium.actions;
 
 import java.util.concurrent.TimeUnit;
 
+/**
+ * Provides wait interaction methods for Minium expressions.
+ *
+ * @author rui.figueira
+ *
+ * @param <T>
+ */
 public interface WaitInteractable<T extends Interactable<?>> extends Interactable<T> {
 
+    /**
+     * Waits for the existence of the elements expression, that is, that it evaluates into a
+     * non-empty set before a timeout occurs. It will fail with a {@link TimeoutException}
+     * if no element matches this expression.
+     *
+     * @return this {@link Interactable}
+     * @throws TimeoutException in case timeout occurs
+     */
     public T waitForExistence();
 
+    /**
+     * Equivalent to {@code elems.withWaitingPreset(waitingPreset).waitForExistence()}.
+     * @param waitingPreset waiting preset to use when waiting for the existence of this
+     * elements expression
+     *
+     * @return this {@link Interactable}
+     * @throws TimeoutException in case timeout occurs
+     */
     public T waitForExistence(String waitingPreset);
 
+    /**
+     * Waits for the unexistence of the elements expression, that is, that it evaluates into a
+     * empty set before a timeout occurs. It will fail with a {@link TimeoutException}
+     * if some element matches this expression.
+     *
+     * @return this {@link Interactable}
+     * @throws TimeoutException in case timeout occurs
+     */
     public T waitForUnexistence();
 
+    /**
+     * Equivalent to {@code elems.withWaitingPreset(waitingPreset).waitForUnexistence()}.
+     * @param waitingPreset waiting preset to use when waiting for the unexistence of this
+     * elements expression
+     *
+     * @return this {@link Interactable}
+     * @throws TimeoutException in case timeout occurs
+     */
     public T waitForUnexistence(String waitingPreset);
 
+    /**
+     * Waits for the existence of the elements expression, that is, that it evaluates into a
+     * non-empty set before a timeout occurs. It will return true in case it succeeds, false
+     * otherwise. No {@link TimeoutException} is thrown in this method.
+     *
+     * @return true if it evaluates into some element, false otherwise
+     */
     public boolean checkForExistence();
 
+    /**
+     * Equivalent to {@code elems.withWaitingPreset(waitingPreset).checkForExistence()}.
+     * @param waitingPreset waiting preset to use when waiting for the unexistence of this
+     * elements expression
+     *
+     * @return true if it evaluates into some element, false otherwise
+     */
     public boolean checkForExistence(String waitingPreset);
 
+    /**
+     * Waits for the unexistence of the elements expression, that is, that it evaluates into a
+     * empty set before a timeout occurs. It will return true in case it succeeds, false
+     * otherwise. No {@link TimeoutException} is thrown in this method.
+     *
+     * @return true if it doesn't evaluates into some element, false otherwise
+     */
     public boolean checkForUnexistence();
 
+    /**
+     * Equivalent to {@code elems.withWaitingPreset(waitingPreset).checkForUnexistence()}.
+     * @param waitingPreset waiting preset to use when waiting for the unexistence of this
+     * elements expression
+     *
+     * @return true if it doesn't evaluates into some element, false otherwise
+     */
     public boolean checkForUnexistence(String waitingPreset);
 
+    /**
+     * Waits for the specified time interval.
+     *
+     * @param time time to wait
+     * @param timeUnit time units for the specified time
+     * @return this {@link Interactable}
+     */
     public T waitTime(long time, TimeUnit timeUnit);
-
 }
