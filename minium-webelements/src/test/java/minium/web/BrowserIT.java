9b5e2902f79f473e964ae5ee33aa45bd518b9995
Rui Figueira <rui.figueira@gmail.com>
2015-06-15 09:51:14 +0100

    browser.root().find(...) replaced with browser.$(...)

--- minium-webelements/src/test/java/minium/web/BrowserIT.java
+++ minium-webelements/src/test/java/minium/web/BrowserIT.java
@@ -53,13 +53,13 @@ public class BrowserIT {
     public void googleSpreadsheet() {
         browser.get("https://docs.google.com/spreadsheet/ccc?key=0Al0ulrJIDCUVdHJjWnJsbG5hY3hBWFp0Vy1OQV9qQUE#gid=0");
 
-        DefaultWebElements colC   = browser.root().find("#0-grid-table-quadrantcolumn-head-section th").withText("C");
-        DefaultWebElements row5   = browser.root().find(".row-header-wrapper").withText("5");
-        DefaultWebElements cellC5 = browser.root().find("#0-grid-table-quadrantscrollable td").below(colC).rightOf(row5);
+        DefaultWebElements colC   = browser.$("#0-grid-table-quadrantcolumn-head-section th").withText("C");
+        DefaultWebElements row5   = browser.$(".row-header-wrapper").withText("5");
+        DefaultWebElements cellC5 = browser.$("#0-grid-table-quadrantscrollable td").below(colC).rightOf(row5);
 
         cellC5.doubleClick();
 
-        DefaultWebElements cellInput = browser.root().find(".cell-input").overlaps(cellC5);
+        DefaultWebElements cellInput = browser.$(".cell-input").overlaps(cellC5);
 
         cellInput.fill("Minium can!");
     }
--- minium-webelements/src/test/java/minium/web/WebFinderIT.java
+++ minium-webelements/src/test/java/minium/web/WebFinderIT.java
@@ -44,7 +44,7 @@ public class WebFinderIT {
     public void testByName() throws Exception {
         browser.get("http://www.google.com");
 
-        DefaultWebElements searchFld = browser.root().find("q");
+        DefaultWebElements searchFld = browser.$("q");
 
         searchFld.fill("Minium Can!");
     }
@@ -53,13 +53,13 @@ public class WebFinderIT {
     public void googleSpreadsheet() {
         browser.get("https://docs.google.com/spreadsheet/ccc?key=0Al0ulrJIDCUVdHJjWnJsbG5hY3hBWFp0Vy1OQV9qQUE#gid=0");
 
-        DefaultWebElements colC   = browser.root().find("#0-grid-table-quadrantcolumn-head-section th").withText("C");
-        DefaultWebElements row5   = browser.root().find(".row-header-wrapper").withText("5");
-        DefaultWebElements cellC5 = browser.root().find("#0-grid-table-quadrantscrollable td").below(colC).rightOf(row5);
+        DefaultWebElements colC   = browser.$("#0-grid-table-quadrantcolumn-head-section th").withText("C");
+        DefaultWebElements row5   = browser.$(".row-header-wrapper").withText("5");
+        DefaultWebElements cellC5 = browser.$("#0-grid-table-quadrantscrollable td").below(colC).rightOf(row5);
 
         cellC5.doubleClick();
 
-        DefaultWebElements cellInput = browser.root().find(".cell-input").overlaps(cellC5);
+        DefaultWebElements cellInput = browser.$(".cell-input").overlaps(cellC5);
 
         cellInput.fill("Minium can!");
     }
