414fc32d670af28066e85a9082ad44604d7e72d7
raphael.rodrigues <raphael.rodrigues@vilt-group.com>
2017-05-11 11:02:57 +0100

    Preparing release

--- scripts/prepare-release.sh
+++ scripts/prepare-release.sh
@@ -10,4 +10,4 @@ RELEASE_VERSION=$(echo $CI_COMMIT_REF_NAME | cut -d '/' -f 2)
 TAG=$CI_PROJECT_NAME-$RELEASE_VERSION
 
 echo "Preparing release $RELEASE_VERSION"
-chronic mvn --batch-mode release:prepare release:perform -Dtag=$TAG -DreleaseVersion=$RELEASE_VERSION  -DperformRelease="true" $MAVEN_EXTRA_ARGS $MAVEN_RELEASE_EXTRA_ARGS
+chronic mvn --batch-mode release:prepare -Dtag=$TAG -DreleaseVersion=$RELEASE_VERSION  -DperformRelease="true" $MAVEN_EXTRA_ARGS $MAVEN_RELEASE_EXTRA_ARGS
